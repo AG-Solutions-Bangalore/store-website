8f2107a38d55cba7ec5a4379fe8ac95de574eb08
Sajid-tech <s.khan9430319425@gmail.com>
2025-07-24 18:50:18 +0530

    `bottom bar , navbar categories`

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
+import { AppBottombar } from '../components/Navbar/AppBottombar';
 
 const MainLayout = ({ children }) => {
   return (
@@ -18,7 +19,9 @@ const MainLayout = ({ children }) => {
       </main>
 
       <Footer />
+      <AppBottombar/>
     </div>
+   
   );
 };
 
